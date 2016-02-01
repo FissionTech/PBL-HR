e4ad54ba3eefddc0311eacbc7b830e8d7da24798
FissionTech <michael.lindsay.j@gmail.com>
2016-02-01 03:37:47 -0500

    Finished

--- Design/MainWindow.xaml.cs
+++ Design/MainWindow.xaml.cs
@@ -2,10 +2,12 @@
 using Microsoft.Win32;
 using System;
 using System.Data;
+using System.Data.SqlClient;
 using System.Data.SQLite;
 using System.IO;
 using System.Text;
 using System.Windows;
+using System.Windows.Controls;
 //using CefSharp;
 
 namespace Design {
@@ -20,7 +22,8 @@ namespace Design {
             //Cef.Initialize(new CefSettings());
             //}
             StatesArray.PopulateArray();
-            
+            Users.SelectionMode = DataGridSelectionMode.Single;
+            Users.SelectionUnit = DataGridSelectionUnit.Cell;
             LoadTemplate("Resources/ReportBasic.txt");
         }
 
@@ -32,7 +35,9 @@ namespace Design {
         /// </summary>
         SQLite.SQLiteConnection DB;
         String DB_Connection;
+        DataSet DataSet;
         String Template;
+
 
         /// <summary>
         /// Updates the DataGridView (Users) with any new information in the DB, as
@@ -41,14 +46,28 @@ namespace Design {
         public void SetView () {
 
             System.Data.SQLite.SQLiteConnection con = new System.Data.SQLite.SQLiteConnection("Data Source=" + DB_Connection + ";Version=3;");
-            DataSet dataSet = new DataSet();
+            DataSet = new DataSet();
             SQLiteDataAdapter dataAdapter = new SQLiteDataAdapter("SELECT * FROM Members", con);
-            dataAdapter.Fill(dataSet);
+            dataAdapter.AcceptChangesDuringFill = false;
+            dataAdapter.Fill(DataSet);
+            DataSet.AcceptChanges();
 
-            Users.ItemsSource = dataSet.Tables[0].DefaultView;
+            Users.DataContext = DataSet.Tables[0].DefaultView;
             con.Close();
             GC.Collect();
 
+        }
+
+        public void SaveDatabase() {
+            DataSet changes = DataSet.GetChanges();
+            System.Data.SQLite.SQLiteConnection con = new System.Data.SQLite.SQLiteConnection("Data Source=" + DB_Connection + ";Version=3;");
+            SQLiteDataAdapter dataAdapter = new SQLiteDataAdapter("SELECT * FROM Members", con);
+            SQLiteCommandBuilder cb = new SQLiteCommandBuilder(dataAdapter);
+            dataAdapter.Update(changes);
+            DataSet.AcceptChanges();
+            con.Close();
+            GC.Collect();
+            
         }
 
         private void LoadTemplate (String loc) {
@@ -61,6 +80,7 @@ namespace Design {
 
         private void SetTemplate (String newTemplate) {
             this.Template = newTemplate;
+
         }
 
         private void SetHTMLView (String document) {
@@ -154,46 +174,40 @@ namespace Design {
             saveFileDialog.Multiselect = false;
 
             if (saveFileDialog.ShowDialog() == true) {
-                if ((dbStream = saveFileDialog.OpenFile()) != null) {
+                if (saveFileDialog.FileName != DB_Connection) {
+                    if ((dbStream = saveFileDialog.OpenFile()) != null) {
 
-                    if (File.Exists(saveFileDialog.FileName)) {
-                        // !!! Must close the stream
-                        dbStream.Close();
+                        if (File.Exists(saveFileDialog.FileName)) {
 
-                        // Have the connection string available for .NET SQLite
-                        DB_Connection = saveFileDialog.FileName;
-                        // Update the Data Grid View (Users)
-                        SetView();
+
+                            // !!! Must close the stream
+                            dbStream.Close();
+
+                            // Load the database into a variable database
+                            DB = new SQLite.SQLiteConnection(saveFileDialog.FileName);
+
+                            // Have the connection string available for .NET SQLite
+                            DB_Connection = @saveFileDialog.FileName;
+                            // Update the Data Grid View (Users)
+                            SetView();
+                        }
                     }
-
+                } else {
+                    MessageBox.Show("You cannot open the currently open file.");
                 }
             }
         }
 
         private void SaveMenuItem_Click (object sender, RoutedEventArgs e) {
-            Stream dbStream;
-            OpenFileDialog saveFileDialog = new OpenFileDialog();
+            
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
 
             saveFileDialog.Filter = "SQLite files (*.sqlite)|*.sqlite";
             saveFileDialog.FilterIndex = 2;
             saveFileDialog.RestoreDirectory = true;
 
             if (saveFileDialog.ShowDialog() == true) {
-                if ((dbStream = saveFileDialog.OpenFile()) != null) {
-
-                    if (File.Exists(saveFileDialog.FileName)) {
-                        // !!! Must close the stream
-                        dbStream.Close();
-
-                        // Load the database into a variable database
-                        DB = new SQLite.SQLiteConnection(saveFileDialog.FileName);
-                        // Have the connection string available for .NET SQLite
-                        DB_Connection = saveFileDialog.FileName;
-                        // Update the Data Grid View (Users)
-                        SetView();
-                    }
-
-                }
+                SaveDatabase();
             }
         }
 
@@ -218,17 +232,64 @@ namespace Design {
                 rp.Append("</table>");
             }
 
+            int activeMembers = DB.Query<Members>("SELECT * FROM Members WHERE Active=1").ToArray().Length;
+            int nonActiveMembers = DB.Query<Members>("SELECT * FROM Members WHERE Active = 0").ToArray().Length;
+            var owingMembers = DB.Query<Members>("SELECT * FROM Members WHERE AmountOwed > 0");
+            float totalOwing = 0f;
+            foreach (var m in owingMembers) {
+                totalOwing += m.AmountOwed;
+            }
             // Replace the 'variable' sections of the HTML
             String revisedTemplate;
-            revisedTemplate = Template.Replace("#TITLE", "Quick Report");
+            revisedTemplate = Template.Replace("@TITLE", "Report");
             revisedTemplate = Template.Replace("@BODY", rp.ToString());
+            revisedTemplate = revisedTemplate.Replace("@ACTIVE_MEMBERS", activeMembers.ToString());
+            revisedTemplate = revisedTemplate.Replace("@NONACTIVE_MEMBERS", nonActiveMembers.ToString());
+            revisedTemplate = revisedTemplate.Replace("@TOTAL_AMOUNT_OWED", totalOwing.ToString());
+            revisedTemplate = revisedTemplate.Replace("@TOTAL_STUDENTS_OWING", owingMembers.ToArray().Length.ToString());
             SetHTMLView(revisedTemplate);
-            // DEBUG: Clipboard.SetText(rp.ToString());
         }
 
         private void LoadTemplate_Click (object sender, RoutedEventArgs e) {
             String file = GetFile("txt");
             LoadTemplate(file);
+            // Loop through each all 50 states and adds appropriate HTML code for all
+            // entries it finds that belong to that state
+            StringBuilder rp = new StringBuilder();
+            foreach (var i in StatesArray.AbbrevToState) {
+                rp.Append("<h1 class='state' id='" + i.Value + "'>" + i.Value + "</h1>");
+                rp.Append("<table class='table table-striped'> <tr>");
+                // Loops through all the column names and creates an appropriate HTML Table
+                foreach (String s in Globals.MembersColumnNames) {
+                    rp.Append("<th>" + s + "</th>");
+                }
+                rp.Append("</tr>");
+
+                // SQL Query for all members matching the current state (i)
+                var query = DB.Query<Members>("SELECT * FROM Members WHERE State=?", i.Value);
+                foreach (var r in query) {
+                    
+                    rp.Append("<tr> <td>" + r.Id + "</td> <td>" + r.FirstName + "</td> <td>" + r.LastName + "</td> <td>" + r.School + "</td> <td>" + r.State + "</td> <td>" + r.Email + "</td> <td>" + r.Grade + "</td> <td>" + r.AmountOwed + "</td> <td>" + r.Active + "</td> </tr>");
+                }
+                rp.Append("</table>");
+            }
+
+            int activeMembers = DB.Query<Members>("SELECT * FROM Members WHERE Active=1").ToArray().Length;
+            int nonActiveMembers = DB.Query<Members>("SELECT * FROM Members WHERE Active = 0").ToArray().Length;
+            var owingMembers = DB.Query<Members>("SELECT * FROM Members WHERE AmountOwed > 0");
+            float totalOwing = 0f;
+            foreach (var m in owingMembers) {
+                totalOwing += m.AmountOwed;
+            }
+            // Replace the 'variable' sections of the HTML
+            String revisedTemplate;
+            revisedTemplate = Template.Replace("@TITLE", "Report");
+            revisedTemplate = Template.Replace("@BODY", rp.ToString());
+            revisedTemplate = revisedTemplate.Replace("@ACTIVE_MEMBERS", activeMembers.ToString());
+            revisedTemplate = revisedTemplate.Replace("@NONACTIVE_MEMBERS", nonActiveMembers.ToString());
+            revisedTemplate = revisedTemplate.Replace("@TOTAL_AMOUNT_OWED", totalOwing.ToString());
+            revisedTemplate = revisedTemplate.Replace("@TOTAL_STUDENTS_OWING", owingMembers.ToArray().Length.ToString());
+            SetHTMLView(revisedTemplate);
         }
 
         private void NewTemplate_Click (object sender, RoutedEventArgs e) {
@@ -268,6 +329,30 @@ namespace Design {
                 }
 
                 SetView();
+        }
+
+        DataGridCell curCell;
+
+        private void Users_BeginningEdit(object sender, DataGridBeginningEditEventArgs e) {
+            curCell = GetDataGridCell(Users.SelectedCells[0]); // User can only select 1 cell
+
+
+            
+        }
+
+        public DataGridCell GetDataGridCell (DataGridCellInfo cellInfo) {
+            var cellContent = cellInfo.Column.GetCellContent(cellInfo.Item);
+            if (cellContent != null)
+                return (DataGridCell)cellContent.Parent;
+
+            return null;
+        }
+
+        private void Users_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e) {
+            var editedTextBox = (TextBox)e.EditingElement;
+            if(editedTextBox != null) {
+                MessageBox.Show("EDITED");
+            }
         }
     }
 }
